3b84cde5db7fe6a71268b8e8959a22ced8efbc3a
Daniel Silva <cesardaniel14774@gmail.com>
2026-04-27 00:58:22 -0300

    Nova Atualização

--- backend/rotas/leituras.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-from controllers.leituras_controller import LeituraController
-
-leituras_bp = Blueprint('leituras', __name__)
-
-@leituras_bp.route('/calcular', methods=['POST'])
-def calcular():
-    resposta, status = LeituraController.processar_calculo(request.get_json())
-    return jsonify(resposta), status
